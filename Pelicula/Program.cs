a49d79f3475434ecd2fc78e41d6d7ae2545578c3
Ricardo Ibarra <116777631+MiadosC1@users.noreply.github.com>
2023-03-22 02:52:36 +0000

    Cambio 1

--- Pelicula/Program.cs
+++ Pelicula/Program.cs
@@ -7,20 +7,29 @@ namespace Pelicula
     public class Pelicula
     {
         //Campos
-        private string Titulo;
-        private int año;
-        private string pais;
-        private string director;
-        private List<Actor> actores = new List<Actor>();
+        public string Titulo;
+        public int Año;
+        public string Pais;
+        public string Director;
+        public List<Actor> actores = new List<Actor>();
+
+        public Pelicula()
+        {
+        }
 
         //Constructores
-     
+        public Pelicula (string titulo, int año, string pais, string director)
+    {
+        Titulo = titulo;
+        Año = año;
+        Pais = pais;
+        Director = director;
+    }
         //Métodos
      
         public void Imprime()
         {
-     //       Console.WriteLine($"{titulo} ({año})");
-
+            Console.WriteLine($"{Titulo} ({Año} {Pais} {Director})");
         }
 
 
@@ -32,7 +41,11 @@ namespace Pelicula
         private string Nombre;
         private int Año;
         //Constructores
-
+        public Actor(string nombre, int año)
+        {
+            Nombre = nombre;
+            Año = año;
+        }
 
         //Métodos 
         public void Imprime()
@@ -46,11 +59,47 @@ namespace Pelicula
 
     class Program
     {
-
-
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello");
+            List<Pelicula> peliculas = new List<Pelicula>();
+            Pelicula p1 = new Pelicula();
+            p1.Titulo = "El padrino";
+            p1.Año = 1972;
+            p1.Pais = "EEUU";
+            p1.Director = "Francis Ford Coppola";
+            p1.actores.Add(new Actor("Marlon Brando", 1924));
+            p1.actores.Add(new Actor("Al Pacino", 1940));
+            p1.actores.Add(new Actor("James Caan", 1940));
+            peliculas.Add(p1);
+
+            Pelicula p2 = new Pelicula();
+            p2.Titulo = "El padrino 2";
+            p2.Año = 1974;
+            p2.Pais = "EEUU";
+            p2.Director = "Francis Ford Coppola";
+            p2.actores.Add(new Actor("Al Pacino", 1940));
+            p2.actores.Add(new Actor("Robert Duvall", 1931));
+            p2.actores.Add(new Actor("Diane Keaton", 1946));
+            peliculas.Add(p2);
+
+            Pelicula p3 = new Pelicula();
+            p3.Titulo = "El padrino 3";
+            p3.Año = 1990;
+            p3.Pais = "EEUU";
+            p3.Director = "Francis Ford Coppola";
+            p3.actores.Add(new Actor("Al Pacino", 1940));
+            p3.actores.Add(new Actor("Andy García", 1956));
+            p3.actores.Add(new Actor("Mariel Hemingway", 1961));
+            peliculas.Add(p3);
+
+            foreach (Pelicula p in peliculas)
+            {
+                p.Imprime();
+                foreach (Actor a in p.actores)
+                {
+                    a.Imprime();
+                }
+            }
         }
     }
 }
